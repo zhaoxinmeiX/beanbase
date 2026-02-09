4e6c8f87388f3e781ea08e2617adee21934581eb
zhaoxinmei <541902876@qq.com>
2026-02-10 09:14:42 +1300

    refactor: use RouterProvider for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import MainLayout from '@/layouts/MainLayout';
-import DashboardPage from '@/pages/DashboardPage';
-import InventoryPage from '@/pages/InventoryPage';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Navigate to="/dashboard" replace />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="inventory" element={<InventoryPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
